18b1d772a11dc1572ffc3746d475c37f8a3a6e99
agrawaltejas01 <agrawaltejas01@gmail.com>
2024-06-11 22:15:04 +0530

    getGymBatchesForSchedulePage

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -1,6 +1,6 @@
 const SERVER = "https://be.zenfitx.link";
 const LOCAL = "http://localhost:8000";
-const STAGING ="13.122.203.223:8000"
+const STAGING = "https://stag-be.zenfitx.link";
 // const LOCAL = "https://64ad4d4f2beb7c.lhr.life";
 
-export const BE_URL = SERVER;
+export const BE_URL = STAGING;
