ce92129ae1f2edb1bb505443863a8d450672b214
agrawaltejas01 <agrawaltejas01@gmail.com>
2024-06-15 14:44:29 +0530

    rzp key changed

--- src/Pages/checkout/book-now-footer.tsx
+++ src/Pages/checkout/book-now-footer.tsx
@@ -54,7 +54,7 @@ async function displayRazorpay(
     description += `, and plus membership for onePass`;
 
   const options = {
-    key: "rzp_test_1ImIaGjw5Cmceg",
+    key: "rzp_live_d5m3ZneVyX6pu2",
     amount: props.totalAmount * 100,
     currency: "INR",
     name: "ZenFitX",
