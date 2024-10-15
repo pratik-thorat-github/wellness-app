73b321e83b99e64e986a215b2e54e37eeb82ad66
pratik-thorat-github <pratik.thorat01@gmail.com>
2024-10-15 21:10:42 +0530

    razorpay revert

--- src/Pages/checkout/book-now-footer.tsx
+++ src/Pages/checkout/book-now-footer.tsx
@@ -130,27 +130,7 @@ async function displayRazorpay(
     prefill: {
       name: userDetails?.name as string,
       email: `${userDetails?.name as string}@example.com`,
-      contact: `+91${userDetails?.phone}` as string,
-    },
-    config: {
-      display: {
-        blocks: {
-          utib: { //name for UPI block
-            name: "Recommended",
-            instruments: [
-              {
-                method: "upi",
-                flows: ["intent"],
-                apps: ["phonepe"]
-              }
-            ]
-          }
-        },
-        sequence: ["block.utib"],
-        preferences: {
-          show_default_blocks: true // Should Checkout show its default blocks?
-        }
-      }
+      contact: userDetails?.phone as string,
     },
     theme: {
       color: "#61dafb",
