867fabf374fe78454a33bc72bce6aff7617b96d9
pratik-thorat-github <pratik.thorat01@gmail.com>
2024-10-16 22:55:12 +0530

    minor fix

--- src/Pages/checkout/book-now-footer.tsx
+++ src/Pages/checkout/book-now-footer.tsx
@@ -114,6 +114,7 @@ async function displayRazorpay(
     method: {
       upi: true
     },
+    webview_intent: true,
     handler: (response: any) => {
       if (response.razorpay_payment_id) {
         if (props.checkoutType == ECheckoutType.BATCH)
