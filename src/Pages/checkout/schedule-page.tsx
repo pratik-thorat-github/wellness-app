6e5ba2d63d6d911f2a8b2075cfdf592e2457d8fd
Vanshika Singh <vanshikasingh@Vanshikas-MacBook-Air.local>
2025-02-07 20:34:41 +0530

    pre select date changes

--- src/Pages/checkout/schedule-page.tsx
+++ src/Pages/checkout/schedule-page.tsx
@@ -156,7 +156,14 @@ const SchedulePage: React.FC<IClassCheckout> = ({}) => {
 
   useEffect(() => {
     if (gym?.gymId) {
-      if (gym?.isOnlyWeekend) {
+      if (gym.availableDates && gym.availableDates.length > 0) {
+        setSelectedDate(gym.availableDates[0]);
+        _getGymBatchesForDate({
+          id: gym.gymId,
+          date: gym.availableDates[0],
+          activity: selectedActivity,
+        });
+      } else if (gym?.isOnlyWeekend) {
         _getGymBatchesForSchedulePage({
           id: gym.gymId,
           isWeekendOnly: true,
@@ -566,26 +573,43 @@ const SchedulePage: React.FC<IClassCheckout> = ({}) => {
           return {
             number: date.getDate(),
             day: toLetterCase(getDayOfWeek(date)),
-            dateString: dateString 
+            dateString: dateString
           };
         })
       : generateFallbackDates();
 
     return (
-      <Flex
-        flex={1}
-        justify="space-evenly"
-        style={{
-          width: "100%",
-          overflowX: "auto",
-          scrollBehavior: "smooth",
-          scrollbarWidth: "none",
-        }}
-      >
-        {weekDateAndDays.map(({ number, day, dateString }) => (
-          <span key={dateString}> {dateTile(number, day, dateString)} </span>
-        ))}
-      </Flex>
+      <div style={{ 
+        width: "100%",
+        display: "flex",
+        justifyContent: "center"
+      }}>
+        <Flex
+          style={{
+            width: "100%",
+            maxWidth: "100%",
+            overflowX: "auto",
+            scrollBehavior: "smooth",
+            scrollbarWidth: "none",
+            msOverflowStyle: "none",
+            WebkitOverflowScrolling: "touch",
+            gap: "9px",
+            display: "flex",
+            justifyContent: "space-between",
+            // padding: "0"
+          }}
+        >
+          {weekDateAndDays.map(({ number, day, dateString }) => (
+            <span key={dateString} style={{ 
+              flex: "0 0 44px",
+              display: "flex",
+              justifyContent: "center"
+            }}>
+              {dateTile(number, day, dateString)}
+            </span>
+          ))}
+        </Flex>
+      </div>
     );
   }
 
